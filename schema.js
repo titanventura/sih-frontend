d19eec3e4f171f6bfe51728f86f9fbcb93656288
titanventura <aswath7862001@gmail.com>
2020-02-05 10:35:17 +0530

    some changes made to the js and materialize css added

--- schema.js
+++ schema.js
@@ -1,53 +1,67 @@
 
-class Sihdata
-{
-    constructor(timestamp,camera_no,regex_string,image_link)
-    {
-        this.timestamp=timestamp;
-        this.camera_no=camera_no;
-        this.regex_string=regex_string;
-        this.image_link=image_link;
+class Sihdata {
+    constructor(timestamp, camera_no, regex_string, image_link) {
+        this.timestamp = timestamp;
+        this.camera_no = camera_no;
+        this.regex_string = regex_string;
+        this.image_link = image_link;
     }
 }
 
-displayFrontend = function(){
-    const center_container = document.getElementsByClassName("center-container");
-    console.log(center_container);
+displayFrontend = function () {
     const main_container = document.getElementById("some");
+
     console.log(main_container);
     const object_list = callBackend();
 
-    object_list.map(
-        (data_obj)=>{
+    object_list.forEach(
+        (data_obj) => {
+            const children_container = document.createElement("div");
+            children_container.className = "card";
+            main_container.appendChild(children_container);
+
             let camera_header = document.createElement("h2");
-            camera_header.textContent=data_obj.camera_no;
-            main_container.appendChild(camera_header);
+            camera_header.textContent = data_obj.camera_no;
+            children_container.appendChild(camera_header);
+
+            let ts = document.createElement("p");
+            ts.textContent = data_obj.timestamp;
+            ts.style.fontSize = "1.3rem";
+            children_container.appendChild(ts); 
+
+            let rs = document.createElement("p");
+            rs.textContent = data_obj.regex_string;
+            rs.style.fontSize = "1.3rem";
+            children_container.appendChild(rs);
+
+            let image = document.createElement("img");
+            image.src = data_obj.image_link;
+            children_container.appendChild(image);
         }
     );
-    
+
     // Call the callBackend method and add elemets to the DOM through looping.
 }
 
 
-callBackend = function(){
+callBackend = function () {
     // Call data from the backend
-    let timestamp_list = [new Date(Date.now()),new Date(Date.now()),new Date(Date.now()),new Date(Date.now())];
-    let camera_no_list = [1,2,3,4];
-    let regex_string_list = ["TN46AU8877","TN46AU8877","TN46AU8877","TN46AU8877"];
-    let image_link_list = ["https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500","https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500","https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500","https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"  ]
-    
-    
+    let timestamp_list = [new Date(Date.now()), new Date(Date.now()), new Date(Date.now()), new Date(Date.now())];
+    let camera_no_list = [1, 2, 3, 4];
+    let regex_string_list = ["TN46AU8877", "TN46AU8877", "TN46AU8877", "TN46AU8877"];
+    let image_link_list = ["https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", "https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", "https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", "https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"]
+
+
     const object_list = [];
 
-    for(let i=0;i<timestamp_list.length;++i)
-    {
+    for (let i = 0; i < timestamp_list.length; ++i) {
         object_list.push(
-            new Sihdata(timestamp_list[i],camera_no_list[i],regex_string_list[i],image_link_list[i])
+            new Sihdata(timestamp_list[i], camera_no_list[i], regex_string_list[i], image_link_list[i])
         );
     }
 
     // Put the backend data into the Sihdata object
-    
+
 
 
     // return list of SIhdata objects.
